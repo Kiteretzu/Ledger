2683a0564ac5131245b45dfe8a8fc2204a4c3bbf
smarth <smarthverma2003@gmail.com>
2025-08-08 16:16:00 +0530

    added router

--- myLedger/apps/backend/src/index.ts
+++ myLedger/apps/backend/src/index.ts
@@ -14,6 +14,7 @@ import {
   getAllpossibleAttendCodes,
   getAllPossibleSubjectCodes,
 } from "./controllers/worker.controller";
+import router from "./router";
 // import "@repo/redis/main";
 
 dotenv.config();
@@ -23,17 +24,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get("/health", (req, res) => {
-  res.status(200).json({ status: "OK" });
-});
-app.post("/login", loginSimple);
-app.get("/getAttendanceDetails", fetchAttendanceDetails);
-app.get("/getProfile", fetchStudentPersonalInfo);
-app.get("/getAttendanceCode", fetchAttenceCode);
-app.get("/getExamsCode", fetchExamCode);
-app.get("/getSubjectDetails", fetchSubjectDetails); // Assuming this is for subjects
-app.get("/worker3", getAllPossibleSubjectCodes);
-app.get("/worker2", getAllpossibleAttendCodes);
+app.use("/api/v1", router);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
